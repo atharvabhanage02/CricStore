c3c04d29baacd25cb86f6b32b2129e877e86e351
Atharva Bhanage <atharvabhanage2002@gmail.com>
2022-07-17 08:39:51 +0530

    feat : added Logout Feature

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,12 +3,13 @@ import { useCart } from "../../Context/Cart-Context/cart-context";
 import "./navbar.css";
 import { useAuth } from "../../Context/Auth/auth-context";
 import { useProducts } from "../../Context/Product-Context/product-context";
+import { FiLogOut } from "react-icons/fi";
 export const Navbar = () => {
   const { state } = useCart();
   const { wishList, cart } = state;
   const wishListCount = wishList.length;
   const cartCount = cart.length;
-  const { login } = useAuth();
+  const { login, logOutUser } = useAuth();
   const { state: prodState, dispatch: prodDispatch } = useProducts();
   return (
     <nav className="navbar">
@@ -34,11 +35,6 @@ export const Navbar = () => {
         </div>
 
         <div className="user-activity-details">
-          {!login.isLogIn ? (
-            <Link className="navbar-login-btn" to="/login">
-              Login
-            </Link>
-          ) : null}
           <div className="badges">
             <Link to="/Wishlist">
               <div className="icon wishlist-icon far fa-heart"></div>
@@ -56,6 +52,16 @@ export const Navbar = () => {
               {cartCount}
             </span>
           </div>
+          {!login.isLogIn ? (
+            <Link className="navbar-login-btn" to="/login">
+              Login
+            </Link>
+          ) : (
+            <FiLogOut
+              className="icon wishlist-icon"
+              onClick={() => logOutUser()}
+            />
+          )}
         </div>
       </div>
     </nav>
--- src/Context/Auth/auth-context.js
+++ src/Context/Auth/auth-context.js
@@ -1,13 +1,21 @@
 import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { logoutHandler } from "../../Utils/logout";
 const AuthContext = createContext();
 const isTokenPresent = localStorage.getItem("token") ? true : false;
 const AuthProvider = ({ children }) => {
+  const navigate = useNavigate();
   const [login, setLogin] = useState({
     isLogIn: isTokenPresent,
     tokenValue: localStorage.getItem("token"),
   });
+  const logOutUser = async () => {
+    logoutHandler();
+    setLogin({ isLogIn: false });
+    navigate("/");
+  };
   return (
-    <AuthContext.Provider value={{ login, setLogin }}>
+    <AuthContext.Provider value={{ login, setLogin, logOutUser }}>
       {children}
     </AuthContext.Provider>
   );
